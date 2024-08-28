c60c756bc1bf13b1b34567578770a9f5fed31bb5
Gerald <geraldjoly191@gmail.com>
2024-08-27 21:17:03 -0300

    Importación en el routes/index de las routas de Country router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const routerCountry = require('./country.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use('countries',routerCountry)
 
 module.exports = router;
